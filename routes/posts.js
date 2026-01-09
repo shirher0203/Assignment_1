dd652b1bdd9b9f877c53b9154a834f42908866d6
Shir Hershkovich <shither0203@gmail.com>
2026-01-09 18:18:28 +0200

    feat: get post by id endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
-// Sample route for creating a post
+// create post
 router.post('/', (req, res) => {
     postController.createPost(req, res);
 });
 
+// get all posts
 router.get('/', (req, res) => {
     postController.getPosts(req, res);
+});
+
+// get post by id
+router.get('/:id', (req, res) => {
+    postController.getPostById(req, res);
 });
 
 module.exports = router;
